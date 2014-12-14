42a4b005578460fa8e14de7f1b90ad3a0844f457
José Manuel Roldán Villamil <jmroldanv@MacBook-Pro-de-Jose.local>
2014-12-14 12:02:23 +0100

    string4-tiles3

---
+++ spring4-tiles3/src/main/java/org/techzoo/springtiles/controller/SpringTilesController.java
@@ -0,0 +1,34 @@
+package org.techzoo.springtiles.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.techzoo.springtiles.vo.Person;
+
+/**
+ * http://localhost:8080/spring4-tiles3/index
+ * 
+ * @author jmroldanv
+ *
+ */
+@Controller
+public class SpringTilesController {
+
+	@RequestMapping(value="index")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="viewPeson")
+	public ModelAndView viewPersons(Model model) {
+		Map<String, List<Person>> persons = 
+				new HashMap<String, List<Person>>();
+		persons.put("persons", Person.createPersons());
+		return new ModelAndView("personList", persons);
+	}
+}
